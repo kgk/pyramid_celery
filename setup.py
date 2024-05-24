80863e8d43a0b4fe08d20e98140e47067190c387
Kris Kvilekval <kris@viqi.org>
2024-05-23 20:40:25 -0700

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 requires = ['pyramid', 'celery']
 
 setup(name='pyramid_celery',
-      version='4.0.3',
+      version='4.0.4',
       description='Celery integration with pyramid',
       long_description=README + "\n" + CHANGES,
       classifiers=[

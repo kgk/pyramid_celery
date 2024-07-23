656744f82c519242e096d513540efa8fc859c677
Kris Kvilekval <kris@viqi.org>
2024-07-22 21:33:15 -0700

    parens about exc

--- pyramid_celery/loaders.py
+++ pyramid_celery/loaders.py
@@ -98,7 +98,7 @@ def safe_conversion(value):
         return bool(value)
     try:
         return literal_eval(value)
-    except ValueError,SyntaxError,TypeError,MemoryError,RecursionError:
+    except (ValueError,SyntaxError,TypeError,MemoryError,RecursionError):
         pass
     return value
 
